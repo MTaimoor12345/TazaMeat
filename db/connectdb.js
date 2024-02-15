ea31d19d6428b545509a62b6e8ec46dd2b6ca7ec
MTaimoor12345 <125217656+MTaimoor12345@users.noreply.github.com>
2024-02-15 15:40:49 +0500

    mongo cluster connection

--- db/connectdb.js
+++ db/connectdb.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 const connectdb = async (DATABASE_URL)=>{
     try{
         const DB_OPTIONS = {dbName : 'school',};
-        await mongoose.connect(DATABASE_URL,DB_OPTIONS);
-        console.log("connect succecfull");
+        await mongoose.connect("mongodb+srv://TazaMeat:taimoor12345@cluster0.sg6t8jo.mongodb.net/?retryWrites=true&w=majority",DB_OPTIONS);
+        console.log("connect succecful");
     }catch(err){
          console.log(err);
     }
